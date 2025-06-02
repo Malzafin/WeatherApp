2e5397c37a794a90dff89e409897f97115a94eea
Gajus <margaj90@gmail.com>
2025-06-02 15:18:53 +0200

    feat: integracja lokalizacji z pogodą
    Użycie expo-location oraz OpenWeather API

--- src/WeatherContext.tsx
+++ src/WeatherContext.tsx
@@ -23,28 +23,41 @@ export const WeatherProvider = ({ children }: {children: ReactNode}) => {
     const updateWeather = async () => {
         console.log('kliknięto odswieżenie')
         try{
-            const apiKey = 'fab91f1b851104105bf6f56b19676548';
-            const city = 'Warsaw'; 
+            const {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setStatus ('Brak zgody na lokalizację');
+                return;
+            }
 
+            const locationData = await Location.getCurrentPositionAsync({});
+            const { latitude, longitude } = locationData.coords;
+
+            // reverse geocoding - dla miasta
+            const place = await Location.reverseGeocodeAsync({ latitude, longitude });
+            const city = place[0]?.city || place[0]?.region || 'Nieznane miasto';
+           
+
+            const apiKey = 'fab91f1b851104105bf6f56b19676548';
             const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pl`
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=pl`
             );
 
             const data = await response.json();
+            const district = data.name;
 
             setTemperature(data.main.temp.toFixed(1));
-            setLocation(data.name);
+        
+            setLocation(`${city}, ${district}`);
             setDescription(data.weather[0].description);
             setIcon(data.weather[0].icon);
-
-            setStatus('Data downloaded correctly')
+            setStatus('Dane pobrane');
             console.log('Weather data downloaded correctly');
 
         }
 
         catch (error) {
             console.error('Error while receiving weather data:', error);
-            setStatus('Data download error');
+            setStatus('Błąd pobierania danych');
         }
     };
 
