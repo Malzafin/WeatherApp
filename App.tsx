5db60fa4ad3e15e04e84a77dec662d8a6c537aa5
Gajus <margaj90@gmail.com>
2025-06-12 15:31:25 +0200

    przeniesienie logiki pogody do HomeScreen oraz integracja z nawigacja

--- App.tsx
+++ App.tsx
@@ -1,82 +1,11 @@
-import { Dimensions, View, Text, StyleSheet, Button, ScaledSize } from 'react-native';
-import { WeatherProvider, useWeather } from './src/WeatherContext';
-import { useEffect, useState } from 'react';
+import { WeatherProvider} from './src/WeatherContext';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <WeatherProvider>
-      <WeatherContent />
+      <RootNavigator />
     </WeatherProvider>
   );
 }
 
-function WeatherContent() {
-  const weather = useWeather();
-
-  const [screen, setScreen] = useState<ScaledSize>(Dimensions.get('window'));
-  const screenWidth = screen.width;
-  const dynamicFontSize = screenWidth < 400 ? 22 : 26;
-  const styles = createStyles(screen.width, dynamicFontSize);
-
-  useEffect(() => {
-    const onChange = ({ window }: { window: ScaledSize }) => {
-      setScreen(window);
-    };
-
-    const resizeHandler = Dimensions.addEventListener('change', onChange);
-
-    return () => {
-      resizeHandler.remove();
-    };
-  }, []);
-
-  return (
-    <View style={[styles.container, screen.width > screen.height && styles.containerHorizontal]}>
-      <View style={styles.block}>
-        <Text style={styles.header}>Weather App</Text>
-        <Text style={styles.info}>Temperatura: {weather?.temperature}°C </Text>
-        <Text style={styles.info}>Lokalizacja: {weather?.location}</Text>
-      </View>
-
-      <View style={styles.block}>
-        <Button title="Refresh data" onPress={() => weather?.updateWeather()} />
-        <Text style={styles.info}>{weather?.status}</Text>
-      </View>
-    </View>
-  );
-}
-
-const createStyles = (width: number, dynamicFontSize: number) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#e0f7fa',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
-      width: '100%',
-      paddingHorizontal: 20,
-    },
-
-    header: {
-      fontSize: dynamicFontSize,
-      fontWeight: 'bold',
-      marginBottom: 20,
-    },
-
-    info: {
-      fontSize: width > 500 ? 20 : 16,
-      marginBottom: 10,
-    },
-
-    containerHorizontal: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-    },
-
-    block: {
-      margin: 10,
-      alignItems: 'center',
-    },
-  });
---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+export type RootStackParamList = {
+    Home: undefined;
+    Details: { temperature: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator(){
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Details" component={DetailsScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
